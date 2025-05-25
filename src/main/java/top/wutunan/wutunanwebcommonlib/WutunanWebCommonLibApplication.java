7301a231e501fc47cb14d5e3f7ef9455e4b66faf
ground-gun <3280261540@qq.com>
2025-05-25 12:17:40 +0800

    第一次提交

---
+++ src/main/java/top/wutunan/wutunanwebcommonlib/WutunanWebCommonLibApplication.java
@@ -0,0 +1,13 @@
+package top.wutunan.wutunanwebcommonlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WutunanWebCommonLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WutunanWebCommonLibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/wutunan/wutunanwebcommonlib/WutunanWebCommonLibApplicationTests.java
@@ -0,0 +1,13 @@
+package top.wutunan.wutunanwebcommonlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WutunanWebCommonLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
